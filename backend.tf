1bb0bcfcaa8b49c56c5969f108f0b9ca9ffd788b
prasanthkunja <150415557+prasanthkunja@users.noreply.github.com>
2024-02-08 21:58:43 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "prasanth-test"
+    bucket = "prasanth-testz"
     key = "terraform.tfstate"
     region = "us-east-1"
     dynamodb_table = "DynamoDbLock"
